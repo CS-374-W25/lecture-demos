c92fca875e427942b48d4bd209f2ce9bd352ffed
Alexander Guyer <guyera@oregonstate.edu>
2025-01-23 11:49:21 -0800

    1/23 lecture

--- assignment-1-demos/main.c
+++ assignment-1-demos/main.c
@@ -20,13 +20,7 @@ void skip_line(FILE* csv_file) {
 	free(dummy);
 }
 
-struct person person_from_line(FILE* csv_file) {
-	char* row = NULL;
-	size_t n = 0;
-	ssize_t row_length = getline(&row, &n, csv_file);
-	row[row_length - 1] = '\0';
-
-
+struct person person_from_line(char* row) {
 	// Alexander Guyer\01999
 	char* name = strtok(row, ",");
 	char* birth_year = strtok(NULL, ",");
@@ -43,8 +37,6 @@ struct person person_from_line(FILE* csv_file) {
 	// name: "Alexander Guyer"
 	// birth_year: "1999"
 
-	free(row);
-
 	return next_person;
 }
 
@@ -53,14 +45,51 @@ struct node* read_csv_file(FILE* csv_file) {
 	
 	struct node* head = NULL;
 	
-	int i = 0;
-	while (i < 3) {
-		struct person next_person = person_from_line(csv_file);
-		// TODO add next_person into our linked list
-		i++;
+
+	// getline()
+	int more_lines_to_read = 1;
+	while (more_lines_to_read) {
+		char* row = NULL;
+		size_t n = 0;
+		ssize_t row_length = getline(&row, &n, csv_file);
+
+		if (row_length != -1) {
+			row[row_length - 1] = '\0';
+
+			struct person next_person = person_from_line(row);
+
+			struct node* next_node = (struct node*) malloc(sizeof(struct node));
+			next_node->value = next_person;
+			next_node->next = head;
+			head = next_node;
+		} else {
+			more_lines_to_read = 0;
+		}
+
+		free(row);
 	}
 
-	return NULL;
+	return head;
+}
+
+void free_person(struct person p) {
+	free(p.name);
+	p.name = NULL;
+}
+
+void free_linked_list(struct node* head) {
+	struct node* itr = head;
+	while (itr) {
+		// Copy itr into temp pointer
+		struct node* temp = itr;
+
+		// Step itr
+		itr = itr->next;
+		
+		// Free the node and its data that itr point to
+		free_person(temp->value);
+		free(temp);
+	}
 }
 
 int main(int argc, char* argv[]) {
@@ -72,4 +101,7 @@ int main(int argc, char* argv[]) {
 	fclose(csv_file);
 
 	// present the menu to the user
+	
+	free_linked_list(head);
+	head = NULL;
 }
