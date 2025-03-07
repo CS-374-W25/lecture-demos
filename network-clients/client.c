cc30a772f6b7059984bf9095c9255c4c87ad12bb
Alexander Guyer <guyera@oregonstate.edu>
2025-03-07 11:59:35 -0800

    3/6 lecture

--- network-clients/client.c
+++ network-clients/client.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
@@ -24,14 +25,18 @@ int main() {
 	}
 	*/
 
-	struct addrinfo hints;
+	struct addrinfo hints = {0};
 	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_protocol = 0;
-	struct addrinfo* server_addr_list;
+	struct addrinfo* server_addr_list = NULL;
 	// localhost is the "loopback hostname". It means "this machine, the one
 	// that this client program is running on".
 	int getaddrinfo_result = getaddrinfo("localhost", "51728", &hints, &server_addr_list);
+	if (getaddrinfo_result != 0) {
+		printf("Error on getaddrinfo!\n");
+		return 1;
+	}
 	
 	struct addrinfo* itr = server_addr_list;
 	int connect_result = -1;
@@ -49,11 +54,52 @@ int main() {
 	}
 	
 	// TODO Send a message to the server
+	char message[260];
+	printf("Enter a message to send: ");
+	fgets(message, 258, stdin);
+	size_t length = strlen(message);
+	message[length - 1] = '@';
+	message[length] = '@';
+	message[length + 1] = '\0';
+
+	int total_bytes_to_send = strlen(message);
+	int total_bytes_sent = 0;
+	int remaining_bytes_to_send = total_bytes_to_send;
+
+	while (remaining_bytes_to_send > 0) {
+		int bytes_sent = send(socket_fd, message + total_bytes_sent, remaining_bytes_to_send, 0);
+		if (bytes_sent > 0) {
+			remaining_bytes_to_send -= bytes_sent;
+			total_bytes_sent += bytes_sent;
+		} else {
+			// Some error occurred
+			return 1;
+		}
+	}
 	
-	
-	// TODO Receive its response
-	
+	// Receive its response
+	char response[260] = {'\0'};
+	int max_bytes_to_recv = 260;
+	int total_bytes_received = 0;
+	int max_remaining_bytes_to_recv = max_bytes_to_recv;
+	while (strstr(response, "@@") == NULL) {
+		int bytes_read = recv(socket_fd, response + total_bytes_received, max_remaining_bytes_to_recv, 0);
+		if (bytes_read > 0) {
+			max_remaining_bytes_to_recv -= bytes_read;
+			total_bytes_received += bytes_read;
+		} else {
+			// Some error occurred
+			printf("Error on recv!\n");
+			return 1;
+		}
+	}
+
+	length = strlen(response);
+	response[length - 1] = '\0';
+	response[length - 2] = '\0';
+	printf("The server responded with: %s\n", response);
 
 	// TODO Shutdown connection and close the socket FD
-	
+	shutdown(socket_fd, SHUT_RDWR);
+	close(socket_fd);
 }
---
+++ network-clients/server.c
@@ -0,0 +1,91 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <netdb.h>
+
+int main() {
+	int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (socket_fd == -1) {
+		printf("Failed on socket!\n");
+		return 1;
+	}
+
+	// Bind the listening socket to port 51728 and a local network interface
+	struct sockaddr_in addr;
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = INADDR_ANY;
+	addr.sin_port = htons(51728);
+	int bind_result = bind(socket_fd, (struct sockaddr*) &addr, sizeof(addr));
+	if (bind_result == -1) {
+		printf("Failed on bind!\n");
+		return 1;
+	}
+
+	int listen_result = listen(socket_fd, 10);
+	if (listen_result == -1) {
+		printf("Failed on listen!\n");
+		return 1;
+	}
+
+	while (1) {
+		struct sockaddr_in client_addr;
+		socklen_t client_socklen;
+		int communication_socket_fd = accept(socket_fd, (struct sockaddr*) &client_addr, &client_socklen);
+		if (communication_socket_fd == -1) {
+			printf("Error on accept!\n");
+			continue;
+		}
+		char message[260] = {'\0'};
+		int max_bytes_to_recv = 260;
+		int total_bytes_received = 0;
+		int max_remaining_bytes_to_recv = max_bytes_to_recv;
+		while (strstr(message, "@@") == NULL) {
+			int bytes_read = recv(communication_socket_fd, message + total_bytes_received, max_remaining_bytes_to_recv, 0);
+			if (bytes_read > 0) {
+				max_remaining_bytes_to_recv -= bytes_read;
+				total_bytes_received += bytes_read;
+			} else {
+				// Some error occurred
+				return 1;
+			}
+		}
+
+		size_t length = strlen(message);
+		message[length - 1] = '\0';
+		message[length - 2] = '\0';
+
+		char* response;
+		if (strcmp(message, "what's your name?") == 0) {
+			// Respond with "My name is ChatBot9000, the world's smartest chatbot!"
+			response = "My name is ChatBot9000, the world's smartest chatbot!@@";
+		} else if (strcmp(message, "open sesame") == 0) {
+			// Respond with "You guessed the secret passphrase! Here, have a cookie :)"
+			response = "You guessed the secret passphrase! Here, have a cookie :)@@";
+		} else {
+			// Respond with "Sorry! I don't know what that means"
+			response = "Sorry! I don't know what that means@@";
+		}
+
+		int total_bytes_to_send = strlen(response);
+		int total_bytes_sent = 0;
+		int remaining_bytes_to_send = total_bytes_to_send;
+
+		while (remaining_bytes_to_send > 0) {
+			int bytes_sent = send(communication_socket_fd, response + total_bytes_sent, remaining_bytes_to_send, 0);
+			if (bytes_sent > 0) {
+				remaining_bytes_to_send -= bytes_sent;
+				total_bytes_sent += bytes_sent;
+			} else {
+				// Some error occurred
+				printf("Error on send!\n");
+				return 1;
+			}
+		}
+
+		shutdown(communication_socket_fd, SHUT_RDWR);
+		close(communication_socket_fd);
+	}
+}
